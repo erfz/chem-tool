61f765752e5ca1b89a8ddede5cf6ae3f6323484f
Jason Guo <jguo35@live.com>
2017-08-18 02:10:20 -0500

    Accent color changed and TabLayout text bolding

--- app/src/main/java/com/example/riesz/chemicalequationbalancer/MainActivity.java
+++ app/src/main/java/com/example/riesz/chemicalequationbalancer/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.riesz.chemicalequationbalancer;
 
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements BalanceEquationFragment.OnFragmentInteractionListener{
     ChemUtilsPagerAdapter mChemUtilsPagerAdapter;
@@ -23,6 +27,30 @@ public class MainActivity extends AppCompatActivity implements BalanceEquationFr
 
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(mViewPager);
+        LinearLayout tabViewGroup = (LinearLayout)((ViewGroup) tabLayout.getChildAt(0)).getChildAt(0);
+        TextView tabTextView = (TextView) tabViewGroup.getChildAt(1);
+        tabTextView.setTypeface(tabTextView.getTypeface(), Typeface.BOLD);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                LinearLayout tabViewGroup = (LinearLayout)((ViewGroup) tabLayout.getChildAt(0)).getChildAt(tab.getPosition());
+                TextView tabTextView = (TextView) tabViewGroup.getChildAt(1);
+                tabTextView.setTypeface(tabTextView.getTypeface(), Typeface.BOLD);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                LinearLayout tabViewGroup = (LinearLayout)((ViewGroup) tabLayout.getChildAt(0)).getChildAt(tab.getPosition());
+                TextView tabTextView = (TextView) tabViewGroup.getChildAt(1);
+                tabTextView.setTypeface(Typeface.create(
+                        tabTextView.getTypeface(), Typeface.NORMAL), Typeface.NORMAL);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
     @Override
