20bdd8bb6862efaea32dc1d6a9fa1766c916648e
Jason Guo <jguo35@live.com>
2017-09-13 01:28:32 -0400

    Implemented snackbars for error messages and undo clearing equation

--- app/src/main/java/io/github/erfz/chemtool/ChemUtilsFragment.java
+++ app/src/main/java/io/github/erfz/chemtool/ChemUtilsFragment.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -17,7 +19,6 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 
 /**
@@ -38,11 +39,12 @@ public class ChemUtilsFragment extends Fragment implements View.OnClickListener 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private String[] savedEquation = new String[2];
     private boolean mKeyboardState;
     private boolean buttonStateChange;
 
-    private Toast toast = null;
     private ConstraintLayout layout;
+    private CoordinatorLayout coordinatorLayout;
     private EditText LHSEqn;
     private EditText RHSEqn;
     private Button pasteClearButton;
@@ -62,21 +64,19 @@ public class ChemUtilsFragment extends Fragment implements View.OnClickListener 
 
         @Override
         public void afterTextChanged(Editable s) {
-            if (!LHSEqn.getText().toString().isEmpty() || !RHSEqn.getText().toString().isEmpty()){
+            if (!LHSEqn.getText().toString().isEmpty() || !RHSEqn.getText().toString().isEmpty()) {
                 pasteClearButton.setText(R.string.clear_button);
                 buttonStateChange = true;
-            }
-            else{
+            } else {
                 pasteClearButton.setText(R.string.equation_paste_button);
                 buttonStateChange = false;
             }
 
-            for (int i = 0; i < s.length(); ++i){
-                if (i == 0 && Character.isLowerCase(s.charAt(i))){
+            for (int i = 0; i < s.length(); ++i) {
+                if (i == 0 && Character.isLowerCase(s.charAt(i))) {
                     String str = s.subSequence(i, i + 1).toString().toUpperCase();
                     s.replace(i, i + 1, str);
-                }
-                else if (i > 0) {
+                } else if (i > 0) {
                     if (Character.isLowerCase(s.charAt(i)) && (s.charAt(i - 1) == '(' || s.charAt(i - 1) == ')' || s.charAt(i - 1) == ' '
                             || s.charAt(i - 1) == '+' || Character.isLowerCase(s.charAt(i - 1))
                             || Character.isDigit(s.charAt(i - 1)))) {
@@ -119,13 +119,13 @@ public class ChemUtilsFragment extends Fragment implements View.OnClickListener 
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        if (savedInstanceState != null){
+        if (savedInstanceState != null) {
             mKeyboardState = savedInstanceState.getBoolean(ARG_KEYBOARD_STATE, false);
         }
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState){
+    public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         boolean keyboardState = imm.hideSoftInputFromWindow(layout.getWindowToken(), 0);
         outState.putBoolean(ARG_KEYBOARD_STATE, keyboardState);
@@ -138,6 +138,7 @@ public class ChemUtilsFragment extends Fragment implements View.OnClickListener 
         View rootView = inflater.inflate(R.layout.fragment_chem_utils, container, false);
 
         layout = (ConstraintLayout) rootView.findViewById(R.id.balance_equation_layout);
+        coordinatorLayout = (CoordinatorLayout) rootView.findViewById(R.id.coordinatorLayout);
         LHSEqn = (EditText) rootView.findViewById(R.id.left_equation_edittext);
         RHSEqn = (EditText) rootView.findViewById(R.id.right_equation_edittext);
         pasteClearButton = (Button) rootView.findViewById(R.id.paste_clear_button);
@@ -145,21 +146,21 @@ public class ChemUtilsFragment extends Fragment implements View.OnClickListener 
         clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
         imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        buttonStateChange=false;
+        buttonStateChange = false;
 
         balanceButton.setOnClickListener(this);
         pasteClearButton.setOnClickListener(this);
 
-        if (mKeyboardState){
+        if (mKeyboardState) {
             imm.showSoftInput(getActivity().getCurrentFocus(), InputMethodManager.SHOW_FORCED);
         }
         LHSEqn.addTextChangedListener(eqnTextWatcher);
         RHSEqn.addTextChangedListener(eqnTextWatcher);
 
-        layout.setOnTouchListener(new View.OnTouchListener(){
+        layout.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent event){
-                if (event.getAction() == MotionEvent.ACTION_UP){
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
                     layout.requestFocus();
                     imm.hideSoftInputFromWindow(layout.getWindowToken(), 0);
                     return true;
@@ -205,59 +206,43 @@ public class ChemUtilsFragment extends Fragment implements View.OnClickListener 
                 try {
                     equation = EquationBalance.balanceEquation(equation);
                 } catch (Exception e) {
-                    if (toast != null){
-                        toast.cancel();
-                    }
-                    toast = Toast.makeText(getActivity(), "Invalid Equation!", Toast.LENGTH_SHORT);
-                    toast.show();
+                    Snackbar.make(coordinatorLayout, R.string.snackbar_invalid_equation, Snackbar.LENGTH_SHORT).show();
                     return;
                 }
-                if (getActivity().getCurrentFocus() != null){
+                if (getActivity().getCurrentFocus() != null) {
                     getActivity().getCurrentFocus().clearFocus();
                 }
                 EquationDialogFragment f = EquationDialogFragment.newInstance(equation);
                 f.show(getFragmentManager(), "equation dialog");
                 break;
             case R.id.paste_clear_button:
-                if (buttonStateChange){
+                if (buttonStateChange) {
+                    savedEquation[0] = LHSEqn.getText().toString();
+                    savedEquation[1] = RHSEqn.getText().toString();
                     LHSEqn.setText("");
                     RHSEqn.setText("");
-                    if (toast != null){
-                        toast.cancel();
-                    }
-                    toast = Toast.makeText(getActivity(), "Equation Cleared", Toast.LENGTH_SHORT);
-                    toast.show();
+                    Snackbar snackbar = Snackbar.make(coordinatorLayout, R.string.snackbar_equation_cleared, Snackbar.LENGTH_LONG);
+                    snackbar.setAction(R.string.snackbar_undo_clear, new UndoClearEquationListener());
+                    snackbar.show();
                     break;
                 }
                 if (!(clipboard.hasPrimaryClip())) {
-                    if (toast != null){
-                        toast.cancel();
-                    }
-                    toast = Toast.makeText(getActivity(), "Clipboard doesn't contain anything!", Toast.LENGTH_SHORT);
-                    toast.show();
+                    Snackbar.make(coordinatorLayout, R.string.snackbar_clipboard_no_text, Snackbar.LENGTH_SHORT).show();
                 } else if (!(clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN) ||
                         clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_HTML))) {
-                    if (toast != null){
-                        toast.cancel();
-                    }
-                    toast = Toast.makeText(getActivity(), "Clipboard doesn't contain usable text!", Toast.LENGTH_SHORT);
-                    toast.show();
+                    Snackbar.make(coordinatorLayout, R.string.snackbar_clipboard_no_parseable_text, Snackbar.LENGTH_SHORT).show();
                 } else {
                     ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
                     equation = item.coerceToText(getActivity()).toString();
-                    equation = equation.replaceAll("-+", "=").replaceAll("<+","=").replaceAll(">+","=")
+                    equation = equation.replaceAll("-+", "=").replaceAll("<+", "=").replaceAll(">+", "=")
                             .replaceAll("→+", "=").replaceAll("←+", "=")
                             .replaceAll("↔+", "=").replaceAll("⇄+", "=")
                             .replaceAll("⇌+", "=");
                     equation = equation.replaceAll("[^A-Za-z0-9\\+\\(\\)\\[\\]=]", "");
-                    equation = equation.replaceAll("=+","=");
+                    equation = equation.replaceAll("=+", "=");
                     String[] eqnHS = equation.split("=");
                     if (eqnHS.length != 2) {
-                        if (toast != null) {
-                            toast.cancel();
-                        }
-                        toast = Toast.makeText(getActivity(), "Invalid equation!", Toast.LENGTH_SHORT);
-                        toast.show();
+                        Snackbar.make(coordinatorLayout, R.string.snackbar_invalid_equation, Snackbar.LENGTH_SHORT).show();
                         return;
                     }
                     LHSEqn.setText(eqnHS[0]);
@@ -266,6 +251,14 @@ public class ChemUtilsFragment extends Fragment implements View.OnClickListener 
                     RHSEqn.setSelection(RHSEqn.length());
                 }
                 break;
+        }
+    }
+
+    private class UndoClearEquationListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            LHSEqn.setText(savedEquation[0]);
+            RHSEqn.setText(savedEquation[1]);
         }
     }
 
