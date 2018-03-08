e7fb6669ea094e9babe1a2896dcb4f797fcbe1b6
Jason Guo <jguo35@live.com>
2018-03-08 03:43:35 -0500

    ButterKnife in dialog fragment class

--- app/src/main/java/io/github/erfz/chemtool/EquationDialogFragment.java
+++ app/src/main/java/io/github/erfz/chemtool/EquationDialogFragment.java
@@ -13,12 +13,33 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 /**
  * Created by zfrs on 3/8/2018.
  */
 
 public class EquationDialogFragment extends AppCompatDialogFragment {
     private String mEquation;
+    private Unbinder unbinder;
+
+    @BindView(R.id.balanced_equation_tv)
+    TextView equationtv;
+    @BindView(R.id.copy_button)
+    Button copyButton;
+
+    @OnClick(R.id.copy_button)
+    public void onClick(View view) {
+        ClipboardManager clipboard = (ClipboardManager) getActivity()
+                .getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("equation", mEquation);
+        clipboard.setPrimaryClip(clip);
+        Snackbar.make(getActivity().findViewById(R.id.coordinatorLayout),
+                R.string.snackbar_equation_copied, Snackbar.LENGTH_SHORT).show();
+    }
 
     public static EquationDialogFragment newInstance(String equation) {
         EquationDialogFragment fragment = new EquationDialogFragment();
@@ -37,31 +58,25 @@ public class EquationDialogFragment extends AppCompatDialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.dialog_fragment_equation, container, false);
+        View view = inflater.inflate(R.layout.dialog_fragment_equation, container, false);
+        unbinder = ButterKnife.bind(this, view);
 
-        TextView equationtv = (TextView) v.findViewById(R.id.balanced_equation);
         equationtv.setText(mEquation);
         equationtv.setTextIsSelectable(true);
         equationtv.setTextColor(Color.BLACK);
 
-        Button copyButton = (Button) v.findViewById(R.id.copy_button);
-        copyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ClipboardManager clipboard = (ClipboardManager) getActivity()
-                        .getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("equation", mEquation);
-                clipboard.setPrimaryClip(clip);
-                Snackbar.make(getActivity().findViewById(R.id.coordinatorLayout), R.string.snackbar_equation_copied, Snackbar.LENGTH_SHORT).show();
-            }
-        });
-
-        return v;
+        return view;
     }
 
     @Override
     public void onResume() {
         getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         super.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
