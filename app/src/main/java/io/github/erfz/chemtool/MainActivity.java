811bb706e6f5c8f52c22090daeff37ddc0518573
Jason Guo <jguo35@live.com>
2018-03-08 21:50:58 -0500

    Better keyboard closing behavior on page change

--- app/src/main/java/io/github/erfz/chemtool/MainActivity.java
+++ app/src/main/java/io/github/erfz/chemtool/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.inputmethod.InputMethodManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements
-        ChemUtilsFragment.OnFragmentInteractionListener, ConstantsEquationsFragment.OnFragmentInteractionListener {
+        ChemUtilsFragment.OnFragmentInteractionListener,
+        ConstantsEquationsFragment.OnFragmentInteractionListener {
     MainPagerAdapter pagerAdapter;
     @BindView(R.id.view_pager)
     ViewPager viewPager;
@@ -27,6 +29,16 @@ public class MainActivity extends AppCompatActivity implements
                 getSupportFragmentManager(), this);
         viewPager.setAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_IDLE && viewPager.getCurrentItem() != 0) {
+                    ((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
+                            .hideSoftInputFromWindow(viewPager.getWindowToken(), 0);
+                }
+            }
+        });
     }
 
     @Override
