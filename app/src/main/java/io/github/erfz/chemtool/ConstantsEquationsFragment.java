1023d2722a52c1c8fe56b35f33c3bfbed773ac9b
Jason Guo <jguo35@live.com>
2018-03-13 22:43:30 -0400

    Cleanup ConstantsEquationsFragment class

--- app/src/main/java/io/github/erfz/chemtool/ConstantsEquationsFragment.java
+++ app/src/main/java/io/github/erfz/chemtool/ConstantsEquationsFragment.java
@@ -3,72 +3,26 @@ package io.github.erfz.chemtool;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import io.github.kexanie.library.MathView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ConstantsEquationsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ConstantsEquationsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ConstantsEquationsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
-    public ConstantsEquationsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConstantsEquationsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConstantsEquationsFragment newInstance(String param1, String param2) {
-        ConstantsEquationsFragment fragment = new ConstantsEquationsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_constants_equations, container, false);
-        MathView textView = v.findViewById(R.id.constants_mathview);
-        textView.config(
+        MathView mathView = v.findViewById(R.id.constants_mathview);
+        mathView.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorBackground));
+        mathView.config(
                 "MathJax.Hub.Config({\n" +
                         "   CommonHTML: { linebreaks: { automatic: true } },\n" +
                         "   \"HTML-CSS\": { linebreaks: { automatic: true } },\n" +
@@ -112,11 +66,10 @@ public class ConstantsEquationsFragment extends Fragment {
                 "$$\\Delta H = H_{\\textrm{products}} - H_{\\textrm{reactants}}$$" +
                 "$$\\Delta S = S_{\\textrm{products}} - S_{\\textrm{reactants}}$$" +
                 "$$\\Delta G = \\Delta H - T\\Delta S$$";
-        textView.setText(ex);
+        mathView.setText(ex);
         return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -140,16 +93,6 @@ public class ConstantsEquationsFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
